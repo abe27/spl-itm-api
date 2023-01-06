3fb3596dd3593debfef66725631cb63af875941c
Taweechai Yuenyang <krumii.it@gmail.com>
2023-01-06 10:57:56 +0700

    update app version

--- routes/router.go
+++ routes/router.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"fmt"
+
+	"github.com/abe/erp.api/configs"
 	"github.com/abe/erp.api/controllers"
 	"github.com/gofiber/fiber/v2"
 )
@@ -8,6 +11,6 @@ import (
 func Router(c *fiber.App) {
 	c.Get("", controllers.Hello)
 	// Prefix Api
-	r := c.Group("/api/v1")
+	r := c.Group(fmt.Sprintf("/api/%s", configs.APP_VERSION))
 	r.Get("", controllers.Hello)
 }
